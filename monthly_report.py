f203cd05e8d1879af4bf312c9390ef40d86dadf1
CodeWithEhtisham <shamikhan0347@gmail.com>
2023-02-10 14:46:50 +0500

    month report working

--- monthly_report.py
+++ monthly_report.py
@@ -24,7 +24,12 @@ class MonthlyReportWindow(QMainWindow, FORM_MAIN):
         self.update(month=None,year=None)
         
     def Handle_Buttons(self):
-        pass
+        self.select_month.dateChanged.connect(self.month_changed)
+
+    def month_changed(self):
+        month = self.select_month.date().month()
+        year = self.select_month.date().year()
+        self.update(month,year)
 
     def update(self,month,year):
         if not month or not year:
@@ -53,6 +58,40 @@ class MonthlyReportWindow(QMainWindow, FORM_MAIN):
         for index , row in enumerate(report):
             for col_index, item in enumerate(row):
                 self.monthly_accounts_table.setItem(index,col_index,QTableWidgetItem(str(item)))
+
+        start_date = datetime(year, month, 1).strftime('%d/%m/%Y')
+        end_date = datetime(year, month, num_days).strftime('%d/%m/%Y')
+        print(start_date,end_date)
+        monthly_expense_report=self.db.conn.execute(
+            f"SELECT date,hoa,SUM(amount) FROM expenses WHERE date BETWEEN '{start_date}' AND '{end_date}' GROUP BY hoa"
+        ).fetchall()
+        for month in monthly_expense_report:
+            if month[0] not in days:
+                monthly_expense_report.remove(month)
+        print(monthly_expense_report)
+        self.monthly_expense_table.setRowCount(len(monthly_expense_report))
+        for index , row in enumerate(monthly_expense_report):
+            for col_index, item in enumerate(row):
+                self.monthly_expense_table.setItem(index,col_index,QTableWidgetItem(str(item)))
+        amount_received=self.db.select(
+            table_name='transactions',
+            columns='sum(paid_fee)',
+            condition=f"date BETWEEN '{start_date}' AND '{end_date}'"
+        )[0][0]
+        self.lbl_total_amount_received.setText(str(amount_received))
+        remaining=self.db.select(
+            table_name='transactions',
+            columns='sum(remaining_fee)',
+            condition=f"date BETWEEN '{start_date}' AND '{end_date}'"
+        )[0][0]
+        self.lbl_total_amount_remaining.setText(str(remaining))
+        expensees=self.db.select(
+            table_name='expenses',
+            columns='sum(amount)',
+            condition=f"date BETWEEN '{start_date}' AND '{end_date}'"
+        )[0][0]
+        self.lbl_total_expense.setText(str(expensees))
+        self.lbl_net_balance.setText(str(amount_received-expensees))
 
 
 
